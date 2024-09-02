a1a0b5a54efa2a95aa0668578f28a539c428fa35
Diego Morales Aquino <diegoguatedb2002@gmail.com>
2024-09-02 00:04:32 -0600

    feat: dibujar circulo

---
+++ src/circle.h
@@ -0,0 +1,69 @@
+#include "color.h"
+#include "fragment.h"
+#include "framebuffer.h"
+
+class Circle {
+public:
+
+    Circle(int x, int y, int radius, const Color& color)
+        : x(x), y(y), radius(radius), color(color) {}
+
+    // Método para dibujar el círculo relleno en el framebuffer
+    void draw() {
+        // Algoritmo del círculo de Bresenham
+        int x0 = x;
+        int y0 = y;
+        int r = radius;
+        
+        int dx = 0;
+        int dy = r;
+        int d = 1 - r;
+
+        fillCircle(x0, y0, dx, dy);
+
+        // Dibujar el círculo utilizando aritmética entera para la eficiencia
+        while (dx < dy) {
+            if (d < 0) {
+                d += 2 * dx + 3;
+            } else {
+                d += 2 * (dx - dy) + 5;
+                dy--;
+            }
+            dx++;
+
+            fillCircle(x0, y0, dx, dy);
+        }
+    }
+
+private:
+    int x, y;        // Centro del círculo
+    int radius;
+    Color color;
+
+    // dibujar y rellenar el círculo
+    void fillCircle(int cx, int cy, int x, int y) {
+        drawHorizontalLine(cx - x, cx + x, cy + y);
+        drawHorizontalLine(cx - x, cx + x, cy - y);
+        drawHorizontalLine(cx - y, cx + y, cy + x);
+        drawHorizontalLine(cx - y, cx + y, cy - x);
+    }
+
+    // dibujar una línea horizontal entre dos puntos en el framebuffer
+    void drawHorizontalLine(int x1, int x2, int y) {
+        if (y < 0 || y >= FRAMEBUFFER_HEIGHT) return; // Verifica si 'y' está dentro del rango
+
+        // Ajusta los límites para que se mantengan dentro del framebuffer
+        x1 = std::max(0, x1);
+        x2 = std::min(FRAMEBUFFER_WIDTH - 1, x2);
+
+        for (int x = x1; x <= x2; ++x) {
+            setPixel(x, y);
+        }
+    }
+
+    // establecer un píxel en el framebuffer
+    void setPixel(int px, int py) {
+        Fragment f = {static_cast<uint16_t>(px), static_cast<uint16_t>(py), 0.0, color};
+        point(f);
+    }
+};
--- src/framebuffer.h
+++ src/framebuffer.h
@@ -10,7 +10,6 @@ const int FRAMEBUFFER_HEIGHT = 800;
 const int FRAMEBUFFER_SIZE = FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT;
 
 Color framebuffer[FRAMEBUFFER_SIZE];
-std::array<float, FRAMEBUFFER_SIZE> zbuffer;
 
 
 Color clearColor = {0, 0, 0};
@@ -22,7 +21,6 @@ void clear() {
         
         framebuffer[i] = clearColor;
     }
-    std::fill(zbuffer.begin(), zbuffer.end(), 99999.0f);
 }
 
 void point(Fragment f)
@@ -30,10 +28,9 @@ void point(Fragment f)
     int index = f.y * FRAMEBUFFER_WIDTH + f.x;
 
     // Verificar si el índice está dentro de la pantalla
-    if (index >= 0 && index < FRAMEBUFFER_SIZE && f.z < zbuffer[index])
+    if (index >= 0 && index < FRAMEBUFFER_SIZE)
     {
         // Colocar color del pixel (x,y)
         framebuffer[index] = f.color;
-        zbuffer[index] = f.z;
     }
 }
